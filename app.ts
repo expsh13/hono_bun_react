ddc6a7c4b976156f4384e3acc2b86f3445d2ac57
Satoki Hata <satokihata@gmail.com>
2024-09-17 19:47:41 +0900

    add middleware

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
-//...
+
+// ミドルウェア
+app.use(logger());
+
+app.get("/test", (c) => {
+  return c.json({ message: "test" });
+});
 
 export default app; // for Cloudflare Workers or Bun
