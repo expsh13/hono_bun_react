4bd803676da3b374c58b70e4fb5768c2cc3f811a
Satoki Hata <satokihata@gmail.com>
2024-09-18 23:43:24 +0900

    add regexp

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -27,4 +27,18 @@ export const expensesRoute = new Hono()
     const expense = await c.req.valid("json");
     fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
     return c.json(expense);
+  })
+  .get("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const expense = fakeExpenses.find((expense) => expense.id === id);
+    if (!expense) return c.notFound();
+    return c.json({ expense });
+  })
+  .delete("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const index = fakeExpenses.findIndex((expense) => expense.id === id);
+    if (index === -1) return c.notFound();
+
+    const deletedExpense = fakeExpenses.splice(index, 1)[0];
+    return c.json({ expense: deletedExpense });
   });
