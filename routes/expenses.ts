b3398a52fa6f2fddc20bfc3e54eacff935194ea3
Satoki Hata <satokihata@gmail.com>
2024-09-19 08:37:21 +0900

    zod omit type

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -2,22 +2,21 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
-type Expense = {
-  id: number;
-  title: string;
-  amount: number;
-};
+const expenseSchema = z.object({
+  id: z.number().int().positive().min(1),
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
+
+type Expense = z.infer<typeof expenseSchema>;
+
+const createPostSchema = expenseSchema.omit({ id: true });
 
 const fakeExpenses: Expense[] = [
   { id: 1, title: "aadgfa", amount: 120 },
   { id: 2, title: "a", amount: 320 },
   { id: 3, title: "ba", amount: 620 },
 ];
-
-const createPostSchema = z.object({
-  title: z.string().min(3).max(100),
-  amount: z.number().int().positive(),
-});
 
 export const expensesRoute = new Hono()
   .get("/", (c) => {
@@ -26,6 +25,7 @@ export const expensesRoute = new Hono()
   .post("/", zValidator("json", createPostSchema), async (c) => {
     const expense = await c.req.valid("json");
     fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
+    c.status(201);
     return c.json(expense);
   })
   .get("/:id{[0-9]+}", (c) => {
