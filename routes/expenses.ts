e65bbaa8f6b89d9901ba4c7d24dcac738e8351c6
Satoki Hata <satokihata@gmail.com>
2024-09-18 19:59:10 +0900

    add hono zod

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
 type Expense = {
@@ -22,9 +23,8 @@ export const expensesRoute = new Hono()
   .get("/", (c) => {
     return c.json({ expenses: fakeExpenses });
   })
-  .post("/", async (c) => {
-    const data = await c.req.json();
-    const expense = createPostSchema.parse(data);
-    console.log(expense);
+  .post("/", zValidator("json", createPostSchema), async (c) => {
+    const expense = await c.req.valid("json");
+    fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
     return c.json(expense);
   });
