79f97a1741730c4d174bfc0724b26b2b7009cd84
Satoki Hata <satokihata@gmail.com>
2024-09-18 19:46:22 +0900

    add zod

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { z } from "zod";
 
 type Expense = {
   id: number;
@@ -12,12 +13,18 @@ const fakeExpenses: Expense[] = [
   { id: 3, title: "ba", amount: 620 },
 ];
 
+const createPostSchema = z.object({
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
+
 export const expensesRoute = new Hono()
   .get("/", (c) => {
     return c.json({ expenses: fakeExpenses });
   })
   .post("/", async (c) => {
-    const expense = await c.req.json();
+    const data = await c.req.json();
+    const expense = createPostSchema.parse(data);
     console.log(expense);
     return c.json(expense);
   });
